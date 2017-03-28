a53f493faa3633347c9393a9709472b49f1f459f
Shane <shane.richards121@gmail.com>
2017-03-27 23:37:18 -0400

    step one instructions complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path');
 
-module.exports = {
-    entry: './app/index.js',
+module.exports = [{
+    name: 'js',
+    entry: './src/index.js',
+    devtool: 'source-map',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
-};
+}];
